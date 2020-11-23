372996815a4d72c23a4dead248263f352d1a15b1
Alex Rodriguez Huerta <arodriguez2407@gmail.com>
2020-11-22 20:45:29 -0500

    web v 1.0.0

--- api/noticia/controllers/noticia.js
+++ api/noticia/controllers/noticia.js
@@ -1,14 +1,22 @@
 'use strict';
-// const { sanitizeEntity } = require("strapi-utils")
+const { sanitizeEntity } = require("strapi-utils")
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  // async findOneUrl(ctx){
+  /**
+   * Retrieve a record.
+   *
+  //  * @return {Object}
+   */
+
+  // async findOne(ctx) {
   //   const { url } = ctx.params;
-  //   const entity = await strapi.services.noticia.findOneUrl({ url });
-  //   return sanitizeEntity(entity, { model: strapi.models.noticia })
-  // }
+
+  //   const entity = await strapi.services.noticia.findOne({ url });
+  //   return sanitizeEntity(entity, { model: strapi.models.noticia });
+  // },
 };
+
